ec7c8130ff426c110fe42b0e0eed458370e80c0d
Ashwanth A R <ashwanth1109@gmail.com>
2018-12-03 07:51:38 +0530

    Added hawkings recursive joke

---
+++ joke.js
@@ -0,0 +1,12 @@
+//===========================================
+// HERE'S A HACKNEYED JOKE
+//===========================================
+recursion = () => {
+    console.log(`to understand recursion, you need `);
+    recursion();
+};
+console.log(`Stephen Hawking once said, \n`);
+recursion();
+//===========================================
+// HAHA -_- | DO NOT RUN THIS
+//===========================================
