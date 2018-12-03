d503510316e88bbb85a72c1e801fdc573a3a539d
Ashwanth A R <ashwanth1109@gmail.com>
2018-12-03 13:12:13 +0530

    Added a way to flatten a recursive array

---
+++ RecursiveArrayFlatten.js
@@ -0,0 +1,10 @@
+recursiveArray = [[1], 2, [3, [4]], [[5], [[6, [7]]]]];
+
+spreadArray = arr =>
+    arr.reduce((accArray, currentVal) => {
+        return accArray.concat(
+            Array.isArray(currentVal) ? spreadArray(currentVal) : currentVal
+        );
+    }, []);
+
+console.log(spreadArray(recursiveArray));
